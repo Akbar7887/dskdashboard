be491e75d17832453a8f1b9fe5631a42ad53f25e
Akbar <akbar358888@gmail.com>
2022-08-22 09:26:44 +0500

    ok

--- lib/ui.dart
+++ lib/ui.dart
@@ -8,10 +8,10 @@ import 'package:google_fonts/google_fonts.dart';
 class Ui {
 
   static final String company = "ООО  \"DSK BINOKOR\"";
-  // static final String url =
-  //     "http://localhost:8087/api/"; //; //
+  static final String url =
+      "http://localhost:8087/api/"; //; //
 
-  static final String url = "http://146.120.17.76:8087/api/";
+   //static final String url = "http://146.120.17.76:8087/api/";
 
   static final TextStyle fonttext = GoogleFonts.openSans(
     // fontSize: 20,
