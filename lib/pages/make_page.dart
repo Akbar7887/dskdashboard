ed692de9112dd8ffba7a3582bf477ef2178e374e
Akbar <akbar358888@gmail.com>
2023-04-18 09:22:28 +0500

    ok

--- lib/pages/make_page.dart
+++ lib/pages/make_page.dart
@@ -3,7 +3,6 @@ import 'dart:typed_data';
 import 'package:dskdashboard/controller/Controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
